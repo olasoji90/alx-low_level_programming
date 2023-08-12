7046bae08c9e972b3104e7491031c13fd07c4e24
olasoji90 <olasongmusic@gmail.com>
2023-08-11 18:50:43 -0700

    all combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+	for (j = i + 1; j <= 9; j++)
+	{
+	putchar('0' + i);
+	putchar('0' + j);
+
+		if (i < 8 || j < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
