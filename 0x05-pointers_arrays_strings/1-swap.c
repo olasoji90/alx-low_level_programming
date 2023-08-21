72aaa8a1d6a33b9b206b3d18e6e098d6c9eed28f
olasoji90 <olasongmusic@gmail.com>
2023-08-21 14:15:16 -0700

    the length

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - take in two variable integers and swap them
+ * @author Anisah
+ * @a: swaps and stores address of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int swap;
+change = *b;
+*b = *a;
+*a = change;
+}
